8a0375c373918a24f2c6c9aed96b382d109ec4d2
Tomona Sako <sakotomona@gmail.com>
2020-05-02 11:51:31 -0700

    Updates

---
+++ precache-manifest.7887ea75b42abd96e55e173c26b09903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2308fe07bbc120eeeb0efa41f8846b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3020247e003c7023e4c6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58682fb6edbfff4e44bc",
+    "url": "/static/js/2.b917ece7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b917ece7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3020247e003c7023e4c6",
+    "url": "/static/js/main.02466c30.chunk.js"
+  },
+  {
+    "revision": "abab4c365432b96ed57e",
+    "url": "/static/js/runtime-main.5b2fb0cd.js"
+  }
+]);
